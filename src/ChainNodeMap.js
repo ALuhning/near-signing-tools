843ff434f1316ae3f76abfe51799bb15b9cd69b0
Sebastian <smontero@mightywizards.com>
2020-12-04 14:02:50 -0600

    Updated handling of chainIDs to the new CAIP definition

--- src/ChainNodeMap.js
+++ src/ChainNodeMap.js
@@ -1,50 +1,50 @@
 module.exports = {
-  '1eaa0824707c8c16bd25145493bf062aecddfeb56c736f6ba6397f3195f33c9f': {
+  '1eaa0824707c8c16bd25145493bf062a': {
     name: 'Telos Testnet',
     host: 'testnet.telos.caleos.io',
     port: 443
   },
-  '4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11': {
+  '4667b205c6838ef70ff7988f6e8257e8': {
     name: 'Telos Mainnet',
     host: 'telos.caleos.io',
     port: 443
   },
-  aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906: {
+  aca376f206b8fc25a6ed44dbdc66547c: {
     name: 'EOS Mainnet',
     host: 'api.eosn.io',
     port: 443
   },
-  '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4': {
+  '1064487b3cd1a897ce03ae5b6a865651': {
     name: 'WAX Mainnet',
     host: 'wax.eosn.io',
     port: 443
   },
-  d5a3d18fbb3c084e3b1f3fa98c21014b5f3db536cc15d08f9f6479517c6a3d86: {
+  d5a3d18fbb3c084e3b1f3fa98c21014b: {
     name: 'BOS Mainnet',
     host: 'bos.eosn.io',
     port: 443
   },
-  cfe6486a83bad4962f232d48003b1824ab5665c36778141034d75e57b956e422: {
+  cfe6486a83bad4962f232d48003b1824: {
     name: 'MEETONE Mainnet',
     host: 'meetone.eosn.io',
     port: 443
   },
-  '2a02a0053e5a8cf73a56ba0fda11e4d92e0238a4a2aa74fccf46d5a910746840': {
+  '2a02a0053e5a8cf73a56ba0fda11e4d9': {
     name: 'Jungle Testnet',
     host: 'jungle.eosn.io',
     port: 443
   },
-  '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191': {
+  '5fff1dae8dc8e2fc4d5b23b2c7665c97': {
     name: 'Kylin Testnet',
     host: 'kylin.eosn.io',
     port: 443
   },
-  f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12: {
+  f16b1833c747c43682f4386fca9cbb32: {
     name: 'WAX Testnet',
     host: 'waxtest.eosn.io',
     port: 443
   },
-  '33cc2426f1b258ef8c798c34c0360b31732ea27a2d7e35a65797850a86d1ba85': {
+  '33cc2426f1b258ef8c798c34c0360b31': {
     name: 'BOS testnet',
     host: 'bostest.eosn.io',
     port: 443
--- test/SigningTools.test.js
+++ test/SigningTools.test.js
@@ -6,7 +6,7 @@ jest.setTimeout(20000)
 describe('getPublicKeys', () => {
   test('getPublicKeys', async () => {
     const pubKeys = await SigningTools.getPublicKeys(
-      '1eaa0824707c8c16bd25145493bf062aecddfeb56c736f6ba6397f3195f33c9f',
+      '1eaa0824707c8c16bd25145493bf062a',
       'sebastianmb1')
     console.log('Public keys: ', pubKeys)
 
@@ -20,7 +20,7 @@ describe('getPublicKeys', () => {
 describe('verifySignature', () => {
   test('verifySignature', async () => {
     let response = await SigningTools.verifySignature({
-      chainId: '1eaa0824707c8c16bd25145493bf062aecddfeb56c736f6ba6397f3195f33c9f',
+      chainId: '1eaa0824707c8c16bd25145493bf062a',
       account: 'sebastianmb1',
       signature: 'SIG_K1_KhjYaak5p8jcKbE91fruP4tZYdB4ePoEkjtzg8oXGr9mgvPaEKQgchayxmNYnacqzfHQVk54kaN4YoZCWNG6ExD37BeWPx',
       data: 'Message to sign'
@@ -29,7 +29,7 @@ describe('verifySignature', () => {
     expect(response).toBe(true)
 
     response = await SigningTools.verifySignature({
-      chainId: '1eaa0824707c8c16bd25145493bf062aecddfeb56c736f6ba6397f3195f33c9f',
+      chainId: '1eaa0824707c8c16bd25145493bf062a',
       account: 'sebastianmb1',
       signature: 'SIG_K1_KhjYaak5p8jcKbE91fruP4tZYdB4ePoEkjtzg8oXGr9mgvPaEKQgchayxmNYnacqzfHQVk54kaN4YoZCWNG6ExD37BeWPx',
       data: 'Message to sign1'
